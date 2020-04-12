31b6fe5474c155a05e0088cc1c74245575f7d89e
Demi Wu <demiwu96@gmail.com>
2020-04-11 21:27:59 -0400

    update employee role function

--- index.js
+++ index.js
@@ -51,7 +51,11 @@ const startSystem = () => {
                     break;
 
                 case "Update Employee Role":
-                    updateEmployee();
+                    updateEmployeeRole();
+                    break;
+
+                case "Update Employee Manager":
+                    updateEmployeeManager();
                     break;
 
                 case "View All Roles":
@@ -81,33 +85,36 @@ const viewAll = tableName => {
     var query;
     switch (tableName) {
         case "employee":
+            // get employee information from database
             query = "SELECT employee.id, employee.first_name, employee.last_name,department_name, role.title, role.salary FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id"
             connection.query(query, (err, res) => {
                 if (err) throw err;
                 console.log("\n");
                 console.table(res);
-                console.log("--------------------------------------------------");
+                console.log("========================================================================================");
                 startSystem();
             });
             break;
 
         case "role":
+            // get role information from database
             query = "SELECT role.id,role.title, department.department_name FROM role JOIN department ON role.department_id = department.id ORDER BY role.id"
             connection.query(query, (err, res) => {
                 if (err) throw err;
                 console.log("\n");
                 console.table(res);
-                console.log("--------------------------------------------------");
+                console.log("========================================================================================");
                 startSystem();
             });
             break;
 
         case "department":
+            // get department information from database
             connection.query("SELECT * FROM department", (err, res) => {
                 if (err) throw err;
                 console.log("\n");
                 console.table(res);
-                console.log("--------------------------------------------------");
+                console.log("========================================================================================");
                 startSystem();
             });
             break;
@@ -117,6 +124,7 @@ const viewAll = tableName => {
 const viewByDepartment = () => {
     connection.query("SELECT * FROM department", function (err, result) {
         if (err) throw err;
+        // prompt the user for the department 
         inquirer
             .prompt(
                 {
@@ -138,14 +146,14 @@ const viewByDepartment = () => {
                         chosen = result[i];
                     }
                 }
-                console.log(chosen.id);
-                var query = connection.query(
+                // get the employee information in the chosen department
+                connection.query(
                     "SELECT employee.id, employee.first_name, employee.last_name,department.department_name, role.title, role.salary FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id WHERE department.id = ?",
                     [chosen.id], function (err, res) {
                         if (err) throw err;
                         console.log("\n");
                         console.table(res);
-                        console.log("------------------------------------------------------------");
+                        console.log("========================================================================================");
                         startSystem();
                     }
                 );
@@ -203,9 +211,81 @@ const viewByDepartment = () => {
 //     });
 // };
 
+const updateEmployeeRole = () => {
+    connection.query("SELECT * FROM employee", function (err, resultE) {
+        if (err) throw err;
+        connection.query("SELECT * FROM role", function (err, resultR) {
+            if (err) throw err;
+            // prompt user for the employee and role for the update
+            inquirer
+                .prompt([
+                    {
+                        type: "rawlist",
+                        name: "chooseEmployee",
+                        message: "Which empolyee needs to be updated?",
+                        choices: function () {
+                            var choiceArray = [];
+                            for (var i = 0; i < resultE.length; i++) {
+                                choiceArray.push(resultE[i].first_name + " " + resultE[i].last_name);
+                            }
+                            return choiceArray;
+                        },
+                    },
+                    {
+                        type: "rawlist",
+                        name: "chooseRole",
+                        message: "What is this person's new role?",
+                        choices: function () {
+                            var choiceArray = [];
+                            for (var i = 0; i < resultR.length; i++) {
+                                choiceArray.push(resultR[i].title);
+                            }
+                            return choiceArray;
+                        },
+                    }])
+                .then(res => {
+                    // get employee data through matching first name from the database
+                    let nameArr = res.chooseEmployee.split(" ");
+                    let chosenE;
+                    for (var i = 0; i < resultE.length; i++) {
+                        if (resultE[i].first_name === nameArr[0]) {
+                            chosenE = resultE[i];
+                        }
+                    };
+                    // get role through matching name from the database
+                    let chosenR;
+                    for (var i = 0; i < resultR.length; i++) {
+                        if (resultR[i].title === res.chooseRole) {
+                            chosenR = resultR[i];
+                        }
+                    };
+                    // set the employee id with the new role id
+                    connection.query(
+                        "UPDATE employee SET ? WHERE ? ",
+                        [
+                            {
+                                role_id: chosenR.id
+                            },
+                            {
+                                id: chosenE.id
+                            },
+                        ], function (err, res) {
+                            if (err) throw err;
+                            console.log("\n Updated!");
+                            console.log("------------------------------------------------------------");
+                            startSystem();
+                        }
+                    );
+                });
+        });
+    });
+
+};
+
 const addRoles = () => {
     connection.query("SELECT * FROM department", function (err, result) {
         if (err) throw err;
+        // prompt the user for the role information
         inquirer
             .prompt([
                 {
@@ -215,12 +295,12 @@ const addRoles = () => {
                 {
                     type: "number",
                     name: "salary",
-                    message: "Enter the salary for this role: "
+                    message: "Enter the salary for this position: "
                 },
                 {
                     type: "rawlist",
                     name: "chooseDepartment",
-                    message: "Which department is this person in?",
+                    message: "Which department is this position in?",
                     choices: function () {
                         var choiceArray = [];
                         for (var i = 0; i < result.length; i++) {
@@ -231,52 +311,50 @@ const addRoles = () => {
                 }
             ])
             .then(res => {
-                var chosen;
+                // get the chosen department information from the database
+                let chosenD;
                 for (var i = 0; i < result.length; i++) {
                     if (result[i].department_name === res.chooseDepartment) {
-                        chosen = result[i];
+                        chosenD = result[i];
                     }
                 }
-
-                var query = connection.query(
+                // add the new role information to the database
+                connection.query(
                     "INSERT INTO role SET ?",
                     {
                         title: res.roleTitle,
                         salary: res.salary,
-                        department_id: chosen.id
+                        department_id: chosenD.id
                     },
                     function (err, res) {
                         if (err) throw err;
                         console.log('Successfully added!');
-                        console.log("------------------------------------------------------------");
+                        console.log("--------------------------------------------------------------------------");
                         startSystem();
                     }
                 );
-            })
-            .catch(err => {
-                if (err) {
-                    console.log(err);
-                };
             });
     });
 };
 
 const addDepartments = () => {
+    // prompt the user for department information
     inquirer
         .prompt({
             name: "departmentName",
             message: "Enter the department name: "
         })
         .then(res => {
-            var query = connection.query(
+            // add the new department to the database
+            connection.query(
                 "INSERT INTO department SET ?",
                 {
                     department_name: res.departmentName
                 },
                 function (err, res) {
                     if (err) throw err;
-                    console.log("Successfully added");
-                    console.log("------------------------------------------------------------");
+                    console.log("Successfully added!");
+                    console.log("--------------------------------------------------------------------------");
                     startSystem();
                 }
             );
