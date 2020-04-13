2751f10aa5611df4408353cb9bb44da1a7cd0e8c
Demi Wu <demiwu96@gmail.com>
2020-04-12 21:47:22 -0400

    fix seeds file

--- seeds.sql
+++ seeds.sql
@@ -5,25 +5,25 @@ USE company_db;
 
 CREATE TABLE department(
 id INT AUTO_INCREMENT NOT NULL,
-department_name VARCHAR(30),
+department_name VARCHAR(30) NOT NULL,
 PRIMARY KEY (id)
 );
 
 CREATE TABLE role(
 id INT AUTO_INCREMENT NOT NULL,
-title VARCHAR(30),
+title VARCHAR(30) NOT NULL,
 salary DECIMAL(10,2),
-department_id INT,
+department_id INT NOT NULL,
 PRIMARY KEY (id),
 FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee(
 id INT AUTO_INCREMENT NOT NULL,
-first_name VARCHAR(30),
-last_name VARCHAR(30),
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
 role_id INT,
-manager_id INT,
+manager_id INT NULL,
 PRIMARY KEY (id),
 FOREIGN KEY (role_id) REFERENCES role(id)
 )
@@ -31,7 +31,3 @@ FOREIGN KEY (role_id) REFERENCES role(id)
 SELECT * FROM department;
 SELECT * FROM role;
 SELECT * FROM employee;
-
--- show empolyee names with id, title and salary
-SELECT employee.id, employee.first_name, employee.last_name,department.department_name, role.title, role.salary
-FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id;
